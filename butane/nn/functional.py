7b2cdad572926ae9d6c88abaad0b9f0232088ed7
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2025-05-21 17:39:45 +0200

    Pinball loss

--- butane/nn/functional.py
+++ butane/nn/functional.py
@@ -1,10 +1,26 @@
 import torch
 
+
 def gaussian(x: torch.Tensor) -> torch.Tensor:
     return (-x.square()).exp()
 
+
 def squashing(x: torch.Tensor) -> torch.Tensor:
-    return (9 / 8. * torch.sin(x)) + (1 / 8. * torch.sin(3. * x))
+    return (9 / 8.0 * torch.sin(x)) + (1 / 8.0 * torch.sin(3.0 * x))
+
 
 def scaled_tanh(x: torch.Tensor, alpha: float) -> torch.Tensor:
     return alpha * torch.tanh(x)
+
+
+# Losses
+def pinball_loss(input, target, alpha, reduction="mean") -> torch.Tensor:
+    assert reduction in ["mean", "sum", "none"], "Reduction can be mean, sum or none"
+
+    loss = torch.max(alpha * (target - input), (1 - alpha) * (input - target))
+
+    if reduction == "mean":
+        loss = loss.mean()
+    elif reduction == "sum":
+        loss = loss.sum()
+    return loss
