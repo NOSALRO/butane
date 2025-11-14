760076aec7526a5b273c1912d80bb7cdb62c2f64
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2025-11-14 14:57:53 +0100

    Minor structural change to embeedings

--- butane/nn/modules/embeddings.py
+++ butane/nn/modules/embeddings.py
@@ -23,21 +23,21 @@ class SinusoidalEmbeddings(torch.nn.Module):
 
     def __init__(self, d_model: int, max_seq_len: Optional[int] = 1000):
         super().__init__()
-        self.__dummy_param = torch.nn.Parameter(torch.empty(0))
-        self._PE = torch.zeros((max_seq_len, d_model))
+        _pe = torch.zeros((max_seq_len, d_model))
         pos = torch.arange(max_seq_len).unsqueeze(1)
         div_term = torch.pow(10000, torch.arange(0, d_model, 2) / d_model)
-        self._PE[:, 0::2] = torch.sin(pos/div_term)
-        self._PE[:, 1::2] = torch.cos(pos/div_term)
+        _pe[:, 0::2] = torch.sin(pos/div_term)
+        _pe[:, 1::2] = torch.cos(pos/div_term)
+        self.register_buffer("_PE", _pe, persistent=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         seq_len = x.size(1)
-        x = x + self._PE[:seq_len, :].to(self.__dummy_param.device)
+        x = x + self._PE[:seq_len, :]
         return x
 
     @property
     def embeddings(self) -> torch.Tensor:
-        return self._PE.to(self.__dummy_param.device)
+        return self._PE
 
     @property
     def dim(self) -> int:
