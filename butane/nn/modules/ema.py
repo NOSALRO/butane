46436a7d84233ab38fbf536bf348c1e316e35951
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2025-12-02 17:44:00 +0100

    Updated EMA implementation, remove Switch EMA to external function (to switch user need explicitly do it in the training loop)

--- butane/nn/modules/ema.py
+++ butane/nn/modules/ema.py
@@ -1,3 +1,5 @@
+from typing import Generator, Dict, Any
+import contextlib
 import torch
 
 
@@ -9,38 +11,35 @@ class EMA(torch.nn.Module):
         decay: float,
         start_update_at: int = 1,
         update_every: int = 1,
-        switch_every: int = -1,
         exclude_bias: bool = False,
     ):
         super().__init__()
         self.model = model
         self.decay = decay
-        self._step = 0
         self._start_update_at = start_update_at
         self._update_every = update_every
-        self._switch_every = switch_every
         self._exclude_bias = exclude_bias
         self._excluded = set()
 
-        self._param_name_mapping = []
-        self._initialize_buffers_from_model(model)
+        self._step = 0
+        self._shadow = {}
+        self._backup = {}
+        self._param_shadow_pairs = []
 
-    def _check_exclusion(self, name, param):
-        if self._exclude_bias and name.endswith('.bias'):
-            return True
-        return False
+        self._initialize_buffers_from_model()
 
-    def _initialize_buffers_from_model(self, model: torch.nn.Module):
-        for name, param in model.named_parameters():
-            if not param.requires_grad:
-                continue
-            if self._check_exclusion(name, param):
-                self._excluded.add(name)
-                continue
-            buffer_name = name.replace('.', '_')
-            if buffer_name not in self._buffers:  # prevents overwriting after load
-                self.register_buffer(buffer_name, param.detach().clone())
-            self._param_name_mapping.append((buffer_name, param))
+    def _initialize_buffers_from_model(self) -> None:
+        self._param_shadow_pairs = []
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                if self._exclude_bias and name.endswith('.bias'):
+                    continue
+                buffer_name = f"ema_{name.replace('.', '_')}"
+                if not hasattr(self, buffer_name):
+                    self.register_buffer(buffer_name, param.detach().clone())
+                shadow_param = getattr(self, buffer_name)
+                self._shadow[name] = shadow_param
+                self._param_shadow_pairs.append((param, shadow_param))
 
     @torch.no_grad()
     def update(self) -> None:
@@ -49,40 +48,40 @@ class EMA(torch.nn.Module):
             return
         if self._step % self._update_every:
             return
-        for buffer_name, param in self._param_name_mapping:
-            buffer = getattr(self, buffer_name)
-            buffer.mul_(self.decay).add_(param.data, alpha=1 - self.decay)
-        if not (self._step % self._switch_every) and self._switch_every > 0:
-            self.apply()
+        for param, shadow in self._param_shadow_pairs:
+            if shadow.device != param.device:
+                shadow.data = shadow.to(param.device)
+            shadow.lerp_(param, weight=1.0 - self.decay)
+
+    def switch(self) -> None:
+        # Switch EMA: https://arxiv.org/pdf/2402.09240
+        for param, shadow in self._param_shadow_pairs:
+            param.data.copy_(shadow)
 
     def apply(self) -> None:
         # Store the current parameters and apply EMA
-        if hasattr(self, "saved_params"):
+        if len(self._backup) != 0:
             raise RuntimeError("EMA.apply() called twice without undo().")
-        self.saved_params = {
-            name.replace('.', '_'): param.data.clone() 
-            for name, param in self.model.named_parameters() 
-            if param.requires_grad
-        }
-        for buffer_name, param in self._param_name_mapping:
-            param.data.copy_(getattr(self, buffer_name))
+        for name, param in self.model.named_parameters():
+            if name in self._shadow:
+                self._backup[name] = param.detach().clone()
+                param.data.copy_(self._shadow[name])
 
     def undo(self) -> None:
         for name, param in self.model.named_parameters():
-            if not param.requires_grad:
-                continue
-            buffer_name = name.replace('.', '_')
-            if buffer_name in self.saved_params:
-                param.data.copy_(self.saved_params[buffer_name])
-        del self.saved_params
+            if name in self._backup:
+                param.data.copy_(self._backup[name])
+        self._backup = {}
 
-    def load_state_dict(self, state_dict, strict=True):
-        self._initialize_buffers_from_model(self.model)
-        return super().load_state_dict(state_dict, strict=strict)
+    def load_state_dict(self, state_dict: Dict[str, Any], strict: bool = True) -> torch.nn.Module:
+        super().load_state_dict(state_dict, strict=strict)
+        self._initialize_buffers_from_model()
+        return self
 
-    @torch.no_grad()
-    def forward(self, *args, **kwargs):
-        self.apply()
-        model_output = self.model(*args, **kwargs)
-        self.undo()
-        return model_output
+    @contextlib.contextmanager
+    def average_parameters(self) -> Generator[None, None, None]:
+        try:
+            self.apply()
+            yield
+        finally:
+            self.undo()
