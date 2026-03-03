dff5c3218e0cbfa58ec240414f6b167421b9d1d1
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2026-03-03 15:43:01 +0100

    Add is_sequence flag, so the transfroms are applied sequence-wise with vmap

--- butane/data/transforms.py
+++ butane/data/transforms.py
@@ -1,21 +1,32 @@
-from typing import Union, Optional, Callable
+from typing import Callable, Union
 import torch
 
-
 class Transforms:
-
-    def __init__(self, *transformations):
+    def __init__(self, *transformations, is_sequence: bool = False):
         self._transforms = list(transformations)
+        self.is_sequence = is_sequence
+
+    def _forward_single(self, x: torch.Tensor) -> torch.Tensor:
+        transformed = x.clone()
+        for transform in self._transforms:
+            transformed = transform(transformed)
+        return transformed
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
-        tranformed = x.clone()
-        for transform in self._transforms:
-            tranformed = transform(tranformed)
-        return tranformed
+        if not self._transforms:
+            return x
+
+        # If it's a sequence, we vmap the ENTIRE pipeline at once!
+        if self.is_sequence:
+            return torch.vmap(self._forward_single)(x)
+        else:
+            return self._forward_single(x)
 
     def __add__(self, t: Callable):
         self._transforms.append(t)
         return self
 
-    def __getitem__(self, idx: int) -> Callable:
-        return Transforms(*self._transforms[idx])
+    def __getitem__(self, idx: Union[int, slice]):
+        if isinstance(idx, slice):
+            return Transforms(*self._transforms[idx], is_sequence=self.is_sequence)
+        return self._transforms[idx]
